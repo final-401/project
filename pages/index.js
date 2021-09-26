83fe4c135d4b36010c689a318cd85af6d8c15bc5
saadoundhirat <saadoundhirat93@gmail.com>
2021-09-26 11:27:58 +0300

    feat: add header components & images folder

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import SignUp from './SignUp'
-import SignIn from './SignIn'
-import Link from '@mui/material/Link';
-import Supplies from './Supplies'
-import Veterinary from './Veterinary';
-
-
+import Head from "next/head";
+import Link from "next/link";
+import Navbar from "../components/Navbar";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
+      
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-      </main>
-
-     
-    </div>
-  )
+      <Navbar />
+    </>
+  );
 }
